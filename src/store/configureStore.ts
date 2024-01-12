af6cd6b24fba38ad40ef6a70ebd97ae5c6c5b9c2
Okan <okany4404@gmail.com>
2024-01-12 17:20:26 +0300

    Redux-thunk

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,10 +1,13 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { authReducer } from './slices/authSlice';
 import { cartReducer } from "./slices/cartSlice";
+import { postReducer } from "./slices/postSlice";
+
 
 const rootReducer = combineReducers({
     cart: cartReducer,
-    auth: authReducer
+    auth: authReducer,
+    post: postReducer
 });
 
 
